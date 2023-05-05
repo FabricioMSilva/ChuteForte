8e62666f8d1d752aab428c504adfca6df91ec088
FABRICIO MARTINS DA SILVA <87395533+FabricioMSilva@users.noreply.github.com>
2023-05-04 21:04:45 -0300

    projeto mantato com fundo

---
+++ chutecerto/src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css';
+import Img from './imagens/Sidebar.svg'
+export default function Home() {
+    return (
+        <div>
+                <div className='Img-bar'>
+                    <img className='Img' src={Img} alt="" />
+
+                    
+                </div>
+              
+
+        </div>
+    )
+
+}
