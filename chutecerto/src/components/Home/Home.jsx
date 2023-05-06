ef122212feae4ad349c4471195c8e58176fe00ca
FABRICIO MARTINS DA SILVA <87395533+FabricioMSilva@users.noreply.github.com>
2023-05-06 14:01:57 -0300

    Mega-sena ok
    problemas no option desetivado

--- chutecerto/src/components/Home/Home.jsx
+++ chutecerto/src/components/Home/Home.jsx
@@ -1,30 +1,78 @@
 import React, { useState } from "react";
 import './style.css';
 import Img from './imagens/Sidebar.svg';
-
+import Logo from './imagens/Logo.svg';
 export default function Home() {
 
-const [bola1 , setbola1] = useState(4);
+    const [bola1, setbola1] = useState();
+    const [numeroBolas, setnumeroBolas] = useState(6);
+    const[tipoSorteio , setTipoSorteio]=useState("Mega-sena");
 
-const res = bola1.sort(()=> Math.random() - 0.5);
-//const res = bola1
-function numeros(n){
+    //const res = bola1
+    const numeros = (n) => {
+        const numerosLot = Array.from({ length: n }, (_, i) => i + 1)
+        return numerosLot.sort(() => Math.random() - 0.5)
+    }
     return (
-        Array.from({length: n}, (_, i) => i + 1)
-    );
-}
-   return (
+
+        //Mega-sena, Quina, Lotofácil, Lotomania, Timemania e Dia de sorte
+
         <div className="geral">
-                <div className='Img-bar'>
-                    <img className='Img' src={Img} alt="" />
-                  </div>    
-                     <div className="bola1">{res[5]}</div>
-                     <div className="bola1">{res[4]}</div>
-                     <div className="bola1">{res[6]}</div>
-                     <div className="bola1">{res[7]}</div>
-                     <div className="bola1">{res[8]}</div>
-                     <div className="bola1">{res[9]}</div>
-                     <div className="Sorteio"><button onClick={()=>setbola1(numeros(60))}>sortear</button></div>
+            <div className='div1'>
+                <img className='Img' src={Img} alt="" />
+                
+                    <div className="DivSorteio">
+                        <select className="tipoSorteio">
+                    
+                            <option onClick={()=>setTipoSorteio('Mega-sena')}>Mega-sena</option>
+                            <option onClick={()=>setTipoSorteio('Quina')}>Quina</option>
+                            <option onClick={()=>setTipoSorteio('Lotomania')}>Lotomania</option>
+                            <option onClick={()=>setTipoSorteio('Timemania')}>Timemania</option>
+                            <option onClick={()=>setTipoSorteio('Sorte')}>Dia de sorte</option>
+                        </select>
+                    </div>
+                
+                <div className="divLogo"><img src={Logo} alt="" /><p className="titleLogo">MEGA-SENA</p></div>
+            </div>
+
+            <div className="div2">
+                <div className="divQtdBolas"><p className="txtQtdBolas">Quantidade de Bolas:</p>
+                    <select className="qtdBolas">
+                        <option>Escolha:</option>
+                        <option onClick={() => setnumeroBolas(6)}>6 Bolas</option>
+                        <option onClick={() => setnumeroBolas(7)}>7 Bolas</option>
+                        <option onClick={() => setnumeroBolas(8)}>8 Bolas</option>
+                        <option onClick={() => setnumeroBolas(9)}>9 Bolas</option>
+                        <option onClick={() => setnumeroBolas(10)}>10 Bolas</option>
+                    </select>
+                </div>
+                <div className="divSorteio">
+                    {bola1?.map((item, index) => {
+                        if (index < numeroBolas) {
+                            return (<div key={index} className="bola1">{item}</div>)
+                        }
+                    })}
+                </div>
+
+                <div className="divBtn" >
+
+
+                    <div class="container">
+                        <div class="center">
+                            <button className="btn" onClick={() => setbola1(numeros(60))}>
+                                <span ><p className="txt">Sortear</p></span>
+                                <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
+                                    <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+                                    <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+                                </svg>
+
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     );
 
