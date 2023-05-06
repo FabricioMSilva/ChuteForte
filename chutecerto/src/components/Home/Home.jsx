29c0c4bfded80cc8b20ab58886815eeb2bc710c3
FABRICIO MARTINS DA SILVA <87395533+FabricioMSilva@users.noreply.github.com>
2023-05-05 23:48:12 -0300

    array de numeros criados

--- chutecerto/src/components/Home/Home.jsx
+++ chutecerto/src/components/Home/Home.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import './style.css';
-import Img from './imagens/Sidebar.svg'
+import Img from './imagens/Sidebar.svg';
+
 export default function Home() {
+
+const [bola1 , setbola1] = useState(4);
+
+const res = bola1.sort(()=> Math.random() - 0.5);
+//const res = bola1
+function numeros(n){
     return (
-        <div>
+        Array.from({length: n}, (_, i) => i + 1)
+    );
+}
+   return (
+        <div className="geral">
                 <div className='Img-bar'>
                     <img className='Img' src={Img} alt="" />
-
-                    
-                </div>
-              
-
+                  </div>    
+                     <div className="bola1">{res[5]}</div>
+                     <div className="bola1">{res[4]}</div>
+                     <div className="bola1">{res[6]}</div>
+                     <div className="bola1">{res[7]}</div>
+                     <div className="bola1">{res[8]}</div>
+                     <div className="bola1">{res[9]}</div>
+                     <div className="Sorteio"><button onClick={()=>setbola1(numeros(60))}>sortear</button></div>
         </div>
-    )
+    );
 
 }
